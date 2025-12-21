9864f657982daa63c71354c1a207e7514ddfbfb9
Samrat-Debugger <md721023@gmail.com>
2025-12-21 14:08:47 +0600

    hero responsive 100% done

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,7 +4,7 @@ import Features from "./Features";
 
 export default function Hero() {
   return (
-    <div className="grid max-w-1xl mx-auto md:grid-cols-2 items-center p-5 pb-0 bg-base-100 ">
+    <div className="grid max-w-1xl mx-auto md:grid-cols-2 items-center p-5 pb-0 bg-base-100 max-md:gap-10 ">
       <div>
         <Subtitle text="Build Your Plan" />
         <h1 className="text-3xl xs:text-5xl lg:text-7xl font-bold">
@@ -24,8 +24,8 @@ export default function Hero() {
         </button>
       </div>
       <div className="relative">
-        <Features className="alert-warning -right-5 top-20 animate-bounce" />
-        <Features className="alert-error bottom-40 " />
+        <Features className="max-md:grid-cols-1 max-md:-translate-x-1/2 alert-warning max-md:left-1/2 max-md:bottom-40 md:-right-5 md:top-20 animate-bounce " />
+        <Features className="alert-error bottom-40 hidden md:grid " />
         <img src="/assets/hero.webp" className="w-full" />
       </div>
     </div>
