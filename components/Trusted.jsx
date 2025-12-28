8bc1204026782f9a4ecd997495844dda6702f101
Samrat-Debugger <md721023@gmail.com>
2025-12-28 19:39:59 +0600

    trusted section done

---
+++ components/Trusted.jsx
@@ -0,0 +1,38 @@
+export default function Trusted() {
+  return (
+    <section className="py-20">
+      <div className="flex max-w-1xl mx-auto px-5 justify-between ">
+        <h1 className="max-w-50 text-2xl font-bold ">
+          Trusted by companies all over the world
+        </h1>
+        <div className="flex gap-8 items-center ">
+          <img
+            className="max-w-30"
+            src="/assets/brand/logo-1.webp"
+            alt="logo-1"
+          />
+          <img
+            className="max-w-30"
+            src="/assets/brand/logo-2.webp"
+            alt="logo-2"
+          />
+          <img
+            className="max-w-30"
+            src="/assets/brand/logo-3.webp"
+            alt="logo-3"
+          />
+          <img
+            className="max-w-30"
+            src="/assets/brand/logo-4.webp"
+            alt="logo-4"
+          />
+          <img
+            className="max-w-30"
+            src="/assets/brand/logo-5.webp"
+            alt="logo-5"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
